e3e7cd29fdc19fda2d07cef59d952ea6ad8d5479
jlotti2 <jlottinger2@gmail.com>
2024-03-28 15:09:12 -0500

    complete contact-list

--- day-4/contact-list.js
+++ day-4/contact-list.js
@@ -21,9 +21,8 @@
  *         undefined if the fullName does not match any contacts in the list.
  *      4. removeContact(contact): takes a contact object to be removed from 
  *         the contact-list.
- *      5. add a printAllContactNames() Function to your makeContactList() factory. The printAllContactNames() Function should 
- *         return a String formated with all the full-names of the separated 
- *         with a line-break, like so:
+ *      5. takes a contact object to be removed from 
+ *         the contact-list., like so:
  *          
  *         myContacts.printAllContactNames(); // => Max Gaudin
  *                                                  John Fraboni
@@ -42,29 +41,50 @@ function makeContact(id, nameFirst, nameLast) {
 
 
 function makeContactList() {
-    /*
-     * You need something here to hold contacts. See length api for a hint:
-     */
-    var contacts = [];
-    
+    let contacts = [];
+
     return {
-        // we implemented the length api for you //
-        length: function() {
-            return contacts.length;
-        },
+        // Add a contact to the list
         addContact: function(contact) {
             contacts.push(contact);
         },
+
+        // Return the number of contacts
+        length: function() {
+            return contacts.length;
+        },
+
+        // Find a contact by full name
         findContact: function(fullName) {
-
+            const names = fullName.split(' ');
+            for (let i = 0; i < contacts.length; i++) {
+                if (contacts[i].nameFirst === names[0] && contacts[i].nameLast === names[1]) {
+                    return contacts[i];
+                }
+            }
+            return undefined;
         },
-        recomeContact: function(contact) {
 
+        // Remove a contact from the list
+        removeContact: function(contact) {
+            for (let i = 0; i < contacts.length; i++) {
+                if (contacts[i] === contact) {
+                    contacts.splice(i, 1);
+                    break;
+                }
+            }
         },
-        
 
-    }
+        // Print all contact names
+        printAllContactNames: function() {
+            let contactNames = contacts.map(contact => `${contact.nameFirst} ${contact.nameLast}`);
+            return contactNames.join('\n');
+        }
+    };
 }
+
+
+
 
 
 
